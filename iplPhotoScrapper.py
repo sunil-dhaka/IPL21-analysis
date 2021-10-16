488857c9422417b1325cc98432198e52417915d8
elliot <dhaka.sonu.monu@gmail.com>
2021-10-16 08:33:24 +0530

    modify ipl photos scrapper
    - as a sample some photos have been uploaded into imagesIPL folder
    - this asks for pages to scrape
    - these images are thumbnails of videos on iplt20.com website
    - further can be improved for specific variant of thumbnail

--- iplPhotoScrapper.py
+++ iplPhotoScrapper.py
@@ -1,13 +1,33 @@
 import requests
+import os
 
-# current season ipl data url>> gets json data
+if os.path.isdir('imagesIPL'):
+    os.chdir('imagesIPL')
+else:
+    os.makedirs('imagesIPL')
+    os.chdir('imagesIPL')
 
-url='https://api.platform.iplt20.com/content/ipl/video/EN/?'
+pages=int(input('how many pages to scrape? per page has 100 images >'))
+imageData=list()
+for page in range(pages):
+    url='https://api.platform.iplt20.com/content/ipl/video/EN/?pageSize=100&page='+str(page)
+    r=requests.get(url)
+    print('statuscode for page',str(page+1),'is >>> ',r.status_code)
+    data=r.json()['content']
+    if len(data)>0:
+        for image in data:
+            item={
+                'title':image.get('title','noTitle'),
+                'desc':image.get('description','noDescription'),
+                'thumbURL':image.get('thumbnailUrl','noThumbnail'),
+                'thumbCaption':image.get('titleUrlSegment','noCaption')
+            }
+            imageData.append(item)
 
-r=requests.get(url)
+            # store data into imagesIPL
+            if item['thumbURL']!='noThumbnail':
+                with open(item['thumbCaption']+'.jpg','wb') as image:
+                    r=requests.get(item['thumbURL'])
+                    image.write(r.content)
 
-print(r.status_code)
-data=r.json()['content']
 
-print(len(data))
-print(data)
